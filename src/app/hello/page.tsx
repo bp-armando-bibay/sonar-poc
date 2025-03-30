3f3bb3a9f160e18d90bf535c8da26d24b856dccc
bp-armando-bibay <armando.bibay@blackpanda.com>
2025-03-31 00:01:13 +0800

    new sonar host

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -8,7 +8,7 @@ const Hello: React.FC = () => {
   };
   return (
     <div>
-      This should reflect in Sonars
+      This should reflect in Sonar
       <h1>{username}</h1>
       <button id="click-me-button" name="click-me-button" onClick={onClick}>
         Click me
